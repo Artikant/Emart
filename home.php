e8a1763d3f8716e520baf5e90c2373b4dbc7a5af
Arti <artikant232002@gmail.com>
2023-07-23 11:26:06 +0530

    first release of sign up

---
+++ home.php
@@ -0,0 +1,16 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>EMart</title>
+</head>
+<body>
+    <div class="navbar">
+        <nav>
+            <a href="#">Welcome to EMart</a>
+            <a href="#"></a>
+        </nav>
+    </div>
+</body>
+</html>
---
+++ sign_in.php
@@ -0,0 +1,30 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>EMart</title>
+    <link rel="stylesheet" href="sign_in.css">
+</head>
+<body>
+    <img src="logo.png" id="logo" style="height: 80px; width: 200px; text-align:center;display: block;margin-left: auto;margin-right: auto;">
+    <div class="create-account">
+        <h3>Create account</h3>
+        <div class="info">
+            <label for="name">Your name<br></label>
+            <input type="text" class="in" placeholder="First and last name"><br>
+            <label for="number">Mobile number<br></label>
+            <input type="number" class="in" placeholder="Mobile number"><br>
+            <label for="email">Email<br></label>
+            <input type="email" class="in" placeholder="Email"><br>
+            <label for="password">Password<br></label>
+            <input type="password" class="in" placeholder="Password"><br>
+            <button type="submit">Submit</button>
+            <p>Already have an account?<a href="login.php">Login</a></p>
+            <p>Buying for work?<a href="#">Create a free business account</a></p>
+            <br><br>
+            <p>By creating an account or logging in, you agree to EMart's <a href="#">Conditions of use</a> and <a href="#" >Privacy Policy</a></p>
+        </div>
+    </div>
+</body>
+</html>
